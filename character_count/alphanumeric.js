39274eedb545bd58e28cd9028ad383052a95035e
Bharath SN <bharathsn0812@gmail.com>
2020-05-21 16:33:29 +0100

    adding character counter

---
+++ character_count/alphanumeric.js
@@ -0,0 +1,11 @@
+function alphanumericCharCount (str) {
+  var obj = {}
+  for (var char of str) {
+    if (/[a-z0-9]/.test(char)) {
+      obj[char] = ++obj[char] || 1
+    }
+  }
+  return obj
+}
+
+console.log(alphanumericCharCount('6bv337cca7'))
