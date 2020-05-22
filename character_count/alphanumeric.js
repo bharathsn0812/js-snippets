2162dad00f213d0666f80654dfe7046da132df28
Bharath SN <bharathsn0812@gmail.com>
2020-05-22 13:12:37 +0100

    beautify html

--- character_count/alphanumeric.js
+++ character_count/alphanumeric.js
@@ -1,6 +1,7 @@
 function alphanumericCharCount (str) {
   var obj = {}
   for (var char of str) {
+    char = char.toLowerCase()
     if (/[a-z0-9]/.test(char)) {
       obj[char] = ++obj[char] || 1
     }
