55c481e15fc19e4e30d3cafd943b19834e56c295
Bharath SN <bharathsn0812@gmail.com>
2020-05-28 17:11:23 +0100

    code lint fix

--- recursion/someRecursive.js
+++ recursion/someRecursive.js
@@ -8,8 +8,6 @@ function someRecursive (arr, callback) {
 }
 
 // someRecursive([1,2,3,4], isOdd) // true
-// someRecursive([4,6,8,9], isOdd) // true
 // someRecursive([4,6,8], isOdd) // false
 // someRecursive([4,6,8], val => val > 10); // false
-
 console.log(someRecursive([1, 2, 3, 4], isOdd))
